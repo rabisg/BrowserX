245e8291f1d13de8f7e4e3514b4ba11cdb09a880
Anshu <anshu.avinash35@gmail.com>
2013-03-04 18:44:03 +0530

    Added very crude GUI

--- browser-x.hs
+++ browser-x.hs
@@ -1,12 +1,39 @@
-import System.Environment
 import Network.Browser
 import Network.HTTP
+import Graphics.UI.Gtk
  
  -- | 'main' runs the main program
 main = do
-    urls <- getArgs
+    initGUI
+    window <- windowNew
+    set window [windowTitle := "Browser-X"]
+    vb <- vBoxNew False 0
+    containerAdd window vb
+
+    hb <- hBoxNew False 0
+    boxPackStart vb hb PackNatural 0
+
+    txtfield <- entryNew
+    boxPackStart hb txtfield PackNatural 5
+    button <- buttonNew 
+    set button [buttonLabel := "Go"]
+    boxPackStart hb button PackNatural 0
+
+    txtstack <- statusbarNew
+    boxPackStart vb txtstack PackNatural 0
+    id <- statusbarGetContextId txtstack "Line"
+
+    widgetShowAll window
+
+    onPressed button (fetchURL txtfield button txtstack id)
+    onDestroy window mainQuit
+    mainGUI
+
+fetchURL :: Entry -> Button -> Statusbar -> ContextId -> IO ()
+fetchURL fld b stk id = do
+    txt <- entryGetText fld
     (_,rsp) <- browse $ do
         setAllowRedirects True
-        setProxy NoProxy
-        request $ getRequest $ head urls
-    print $ rspBody rsp
+        request $ getRequest txt
+    msgid <- statusbarPush stk id $ rspBody rsp
+    return ()
