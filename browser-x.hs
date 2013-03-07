e493e4d943f0c7ed621eb63d5e088ac2e2b64eb1
Anshu <anshu.avinash35@gmail.com>
2013-03-07 14:53:20 +0530

    Added Back, Forward, Reload and Quit buttons.

--- browser-x.hs
+++ browser-x.hs
@@ -33,12 +33,37 @@ browser url = do
     
     -- Create WebKit view.
     webView <- webViewNew    
+    webViewSetMaintainsBackForwardList webView True
 
-    -- Create window box.
+    -- Create window boxes.
     winBox <- vBoxNew False 0
+    topBox <- hBoxNew False 0
 
     -- Create address bar.
     addressBar <- entryNew
+
+    -- Create toolbar.
+    back <- actionNew "BACK" "Back" (Just "") (Just stockGoBack)
+    forw <- actionNew "FORW" "Forward" (Just "") (Just stockGoForward)
+    relo <- actionNew "RELO" "Reload" (Just "") (Just stockRedo)
+    exit <- actionNew "EXIT" "Exit" (Just "") (Just stockQuit)
+
+    agr <- actionGroupNew "AGR"
+    mapM_ (\act -> actionGroupAddActionWithAccel agr act Nothing)[back,forw,relo,exit]
+
+    ui <- uiManagerNew
+    uiManagerAddUiFromString ui uiDecl
+    uiManagerInsertActionGroup ui agr 0
+
+    maybeToolbar <- uiManagerGetWidget ui "/ui/toolbar"
+    let toolbar = case maybeToolbar of
+                    (Just x) -> x
+                    Nothing -> error "Cannot get toolbar"
+
+    onActionActivate exit (widgetDestroy window)
+    onActionActivate back (webViewGoBack webView)
+    onActionActivate forw (webViewGoForward webView)
+    onActionActivate relo (webViewReload webView)
 
     -- Create scroll window.
     scrollWin <- scrolledWindowNew Nothing Nothing
@@ -78,7 +103,9 @@ browser url = do
         return webView        
 
     -- Connect and show.
-    boxPackStart winBox addressBar PackNatural 0
+    boxPackStart topBox addressBar PackGrow 0
+    boxPackStart topBox toolbar PackGrow 0
+    boxPackStart winBox topBox PackNatural 0
     boxPackStart winBox scrollWin PackGrow 0
     window `containerAdd` winBox
     scrollWin `containerAdd` webView
@@ -86,6 +113,17 @@ browser url = do
     widgetShowAll window
 
     mainGUI
+
+uiDecl=  "<ui>\
+\           <toolbar>\
+\            <toolitem action=\"BACK\" />\
+\            <toolitem action=\"FORW\" />\
+\            <separator />\
+\            <toolitem action=\"RELO\" />\
+\            <separator />\
+\            <toolitem action=\"EXIT\" />\
+\           </toolbar>\
+\          </ui>"
 
 fetchURL :: String -> IO String
 fetchURL url = do
@@ -98,4 +136,4 @@ checkProtocol :: String -> String
 checkProtocol url = 
     case (parseURI url) of
         Nothing     -> "http://" ++ url
-        Just uri    -> if (uriScheme uri == "http:") then url else "http://google.com"
+        Just uri    -> if (uriScheme uri == "http:") then url else error "Protocol not supported"
